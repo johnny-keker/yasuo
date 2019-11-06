a0296ed3a7940f4506183029267e785722c00ba3
johnny-keker <giomail.iv@gmail.com>
2019-11-06 11:50:27 +0300

    [layer_100] parsing n from getopt

--- spf/layer_100/keker_tail.c
+++ spf/layer_100/keker_tail.c
@@ -1,18 +1,22 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int parse_int(const char* str, long* var) {
+  char* end;
+  *var = strtol(str, &end, 10);
+  return 0;
+}
 
 int main(int argc, char *argv[]) {
   int opt = 0;
-  while ((opt = getopt(argc, argv, "hn")) != -1) {
+  long num_lines = 0;
+
+  while ((opt = getopt(argc, argv, "n:")) != -1) {
     switch (opt) {
-      case 'h':
-        write(STDOUT_FILENO, "Hello\n", 7 * sizeof(char));
-        break;
       case 'n':
-        write(STDOUT_FILENO, "DIE\n", 5 * sizeof(char));
-        break;
-      default:
-        write(STDOUT_FILENO, "WhoCares\n", 10 * sizeof(char));
+        parse_int(optarg, &num_lines);
         break;
     }
   }
