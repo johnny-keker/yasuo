9f50cfc7c0d120a4edf1b3258b352e5365818238
johnny-keker <giomail.iv@gmail.com>
2019-11-07 23:01:58 +0300

    [layer_100] c seems working

--- spf/layer_100/keker_tail.c
+++ spf/layer_100/keker_tail.c
@@ -2,11 +2,46 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+
+const int BUFSIZE = 4096;
 
 int parse_int(const char* str, long* var) {
   char* end;
   *var = strtol(str, &end, 10);
   return 0;
+}
+
+char* get_file_contents(int fd) {
+  char buffer[BUFSIZE];
+  char* contents = NULL; 
+  int byte_count, real_buf_size = 0;
+  while ((byte_count = read(fd, buffer, BUFSIZE)) > 0) {
+    contents = realloc(contents, real_buf_size + byte_count);
+    memcpy(contents + real_buf_size, buffer, byte_count);
+    real_buf_size += byte_count;
+  }
+  contents = realloc(contents, real_buf_size + 1);
+  contents[real_buf_size] = '\0';
+  return contents;
+}
+
+void print_last_n_lines(char* contents, int n) {
+  char* end = contents + strlen(contents);
+  char* curr_char;
+  int curr_n = 0;
+  if (*(end - 1) == '\n')
+    curr_n--;
+  for (curr_char = end; curr_char != contents; --curr_char) {
+    if (*curr_char == '\n') {
+      curr_n++;
+      if (curr_n == n)
+        break;
+    }
+  }
+  if (*(curr_char) == '\n')
+    curr_char++;
+  write(STDOUT_FILENO, curr_char, end - curr_char);
 }
 
 int main(int argc, char *argv[]) {
@@ -20,4 +55,15 @@ int main(int argc, char *argv[]) {
         break;
     }
   }
+  char* contents;
+
+  if (optind != argc && *argv[optind] != '-') {
+    int fd = open(argv[optind], O_RDONLY);
+    contents = get_file_contents(fd);
+    close(fd);
+  }
+  else {
+    contents = get_file_contents(STDIN_FILENO);
+  }
+  print_last_n_lines(contents, num_lines);
 }
