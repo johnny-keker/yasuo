3a3f519a52b851b52fe1b86af636a9f5a370e736
johnny-keker <giomail.iv@gmail.com>
2019-11-08 22:55:52 +0300

    [layer_100] perl vanilla tail

--- spf/layer_100/keker_tail.pl
+++ spf/layer_100/keker_tail.pl
@@ -4,16 +4,31 @@ use strict;
 use warnings;
 use Getopt::Long;
 
-my $num_lines   = 10;
+my $num_lines = 10;
 my $word_mode;
+
+# cmd line args parsing
 GetOptions ("n=i" => \$num_lines,
             "w"   => \$word_mode)
 or die("Error in command line arguments\n");
 
 if ($word_mode) {
-  print "word mode\n";
+
 }
 else {
-  print "line mode\n";
+  my @lines;
+  my $processed = 0;
+  while (my $line = <ARGV>) {
+    if ($processed < $num_lines) {
+      push (@lines, $line);
+      $processed++;
+    }
+    else {
+      shift @lines;
+      push (@lines, $line);
+    }
+  }
+  foreach my $line (@lines) {
+    print $line;
+  }
 }
-
