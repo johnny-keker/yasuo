42cea52531693129a357000fb78afef235ed5b62
johnny-keker <giomail.iv@gmail.com>
2019-12-02 23:15:49 +0300

    [layer_101] mmap client

--- spf/layer_101/client.c
+++ spf/layer_101/client.c
@@ -6,6 +6,8 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <stdbool.h>
+#include <sys/mman.h>
+#include <fcntl.h>
 #include "system_info.h"
 
 bool parse_int(const char* str, int* var) {
@@ -20,9 +22,11 @@ int main(int argc, char* argv[]) {
   printf("<...initializing-client...>\n\n");
   // logging
   
+  struct system_info* sys_info;
+  
   // setup client from args
   int opt = 0;
-  while ((opt = getopt(argc, argv, "v:")) != -1) {
+  while ((opt = getopt(argc, argv, "v:m:")) != -1) {
     switch (opt) {
       case 'v':
         // logging
@@ -33,25 +37,33 @@ int main(int argc, char* argv[]) {
         int sysVMemID;
         parse_int(optarg, &sysVMemID);
         printf("    <...id=%u...>\n\n", sysVMemID);
-        struct system_info* sys_info = (struct system_info*)shmat(sysVMemID, NULL, 0);
-        
+        sys_info = (struct system_info*)shmat(sysVMemID, NULL, 0);
+        break;
+      case 'm':
         // logging
-        printf("<...received-data...>\n");
-        printf("    <...time=%lu...>\n", (*sys_info).startup_time);
-        printf("    <...pid=%u...>\n", (*sys_info).pid);
-        printf("    <...uid=%u...>\n", (*sys_info).uid);
-        printf("    <...gid=%u...>\n", (*sys_info).gid);
-        printf("    <...ave-sys-load-1min=%f...>\n", (*sys_info).sys_loads[0]);
-        printf("    <...ave-sys-load-2min=%f...>\n", (*sys_info).sys_loads[1]);
-        printf("    <...ave-sys-load-5min=%f...>\n", (*sys_info).sys_loads[2]);
-        printf("<...received-data...>\n\n");
-        printf("<...terminating-client...>\n");
-        // logging
-        
+        printf("<...running-mmap-mode...>");
+        //logging
+
+        // opening file and getting system_info structure from it
+				int mmapFD = open(optarg, O_RDWR, 0644);  // open file
+        sys_info = (struct system_info*)mmap(NULL, sizeof(struct system_info),
+            PROT_READ, MAP_SHARED, mmapFD, 0); // map file to memory
         break;
       default:
         fprintf(stderr, "Usage: %s [-v]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
   }
+  // logging
+  printf("<...received-data...>\n");
+  printf("    <...time=%lu...>\n", (*sys_info).startup_time);
+  printf("    <...pid=%u...>\n", (*sys_info).pid);
+  printf("    <...uid=%u...>\n", (*sys_info).uid);
+  printf("    <...gid=%u...>\n", (*sys_info).gid);
+  printf("    <...ave-sys-load-1min=%f...>\n", (*sys_info).sys_loads[0]);
+  printf("    <...ave-sys-load-2min=%f...>\n", (*sys_info).sys_loads[1]);
+  printf("    <...ave-sys-load-5min=%f...>\n", (*sys_info).sys_loads[2]);
+  printf("<...received-data...>\n\n");
+  printf("<...terminating-client...>\n");
+  // logging
 }
