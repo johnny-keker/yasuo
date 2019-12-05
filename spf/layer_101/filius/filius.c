7764b4c511d4541a903e797fa53e81c7689e3c02
johnny-keker <giomail.iv@gmail.com>
2019-12-06 01:38:24 +0300

    [layer_101] filius: pthread

--- spf/layer_101/filius/filius.c
+++ spf/layer_101/filius/filius.c
@@ -1,13 +1,25 @@
 #include "alphabet.h"
 #include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void* inv_f(void* args) {
+	int* interval = (int *)args;
+	for (;;) {
+		sleep(*interval);
+		invert_case();
+		printf("\n<...inverted-case...>\n");
+		print_alphabet();
+		printf("\n<...inverted-case...>\n");
+	}
+}
 
 int main() {
-	print_alphabet();
-	printf("\n");
-	invert_case();
-	print_alphabet();
-	printf("\n");
-	swap_alphabet();
-	print_alphabet();
+	pthread_t inv_thread;
+	int inv_interval = 1;
+	
+	pthread_create(&inv_thread, NULL, inv_f, (void *)&inv_interval);
+	pthread_join(inv_thread, NULL);
+
 	return 0;
 }
