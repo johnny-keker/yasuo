f762566f29f92f6ebb4aea9560bc08b2a7d78cb0
johnny-keker <giomail.iv@gmail.com>
2019-12-06 00:43:28 +0300

    [layer_101] filius: comments

--- spf/layer_101/filius/alphabet.c
+++ spf/layer_101/filius/alphabet.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <ctype.h>
 
-
+// defining the alphabet array
 char alphabet[26] = {
 	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
  	'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 };
 
+// function for printing alphabet
 void print_alphabet() {
 	char* alpha = alphabet;
 	while(*alpha) {
@@ -15,6 +16,7 @@ void print_alphabet() {
 	}
 }
 
+// function for inerting the case of alphabet
 void invert_case() {
 	for (int i = 0; i < 27; ++i) {
 		alphabet[i] = (islower(alphabet[i])) 
