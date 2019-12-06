1cc811bd68ae44206cd5b0ad61aa7a05ce01cf00
johnny-keker <giomail.iv@gmail.com>
2019-12-06 17:58:49 +0300

    [layer_101] filius: comments

--- spf/layer_101/filius/filius.c
+++ spf/layer_101/filius/filius.c
@@ -26,13 +26,16 @@ void* swp_f(void* args) {
 }
 
 int main() {
+  // thread descriptors init
 	pthread_t inv_thread, swp_thread;
+  // setting up intervals (hardcoded for now)
 	int inv_interval = 100, swp_interval = 200;
 	
+  // THREADS INIT
 	pthread_create(&inv_thread, NULL, inv_f, (void *)&inv_interval);
 	pthread_create(&swp_thread, NULL, swp_f, (void *)&swp_interval);
+  // THREADS START
 	pthread_join(inv_thread, NULL);
 	pthread_join(swp_thread, NULL);
-
 	return 0;
 }
