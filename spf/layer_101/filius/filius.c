dbbdb4903e53c5661462c4792c42f12ae7f9adcb
johnny-keker <giomail.iv@gmail.com>
2019-12-06 18:23:07 +0300

    [layer_101] filius: ifdef MUTEX

--- spf/layer_101/filius/filius.c
+++ spf/layer_101/filius/filius.c
@@ -3,19 +3,25 @@
 #include <pthread.h>
 #include <unistd.h>
 
+#ifdef MUTEX
 // define mutex
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+#endif
 
 void* inv_f(void* args) {
 	int* interval = (int *)args;
 	for (;;) {
 		usleep(*interval);
+#ifdef MUTEX
     pthread_mutex_lock(&mutex);
+#endif
 		invert_case();
 		printf("\n<...inverted-case...>\n");
 		print_alphabet();
 		printf("\n<...inverted-case...>\n");
+#ifdef MUTEX
     pthread_mutex_unlock(&mutex);
+#endif
 	}
 }
 
@@ -23,12 +29,16 @@ void* swp_f(void* args) {
 	int* interval = (int *)args;
 	for (;;) {
 		usleep(*interval);
+#ifdef MUTEX
     pthread_mutex_lock(&mutex);
+#endif
 		swap_alphabet();
 		printf("\n<...swap...>\n");
 		print_alphabet();
 		printf("\n<...swap...>\n");
+#ifdef MUTEX
     pthread_mutex_unlock(&mutex);
+#endif
 	}
 }
 
@@ -37,10 +47,10 @@ int main() {
 	pthread_t inv_thread, swp_thread;
   // setting up intervals (hardcoded for now)
 	int inv_interval = 100, swp_interval = 200;
-	
+#ifdef MUTEX	
   // initialize mutex
   pthread_mutex_init(&mutex, NULL);
-
+#endif
   // THREADS INIT
 	pthread_create(&inv_thread, NULL, inv_f, (void *)&inv_interval);
 	pthread_create(&swp_thread, NULL, swp_f, (void *)&swp_interval);
